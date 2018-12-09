ea5e1e2ee223671821671d4d6f98bfa18c65b1bb
Scott Stadt <scott.stadt@gmail.com>
2018-12-09 17:43:51 -0500

    progress commit; tinkering with icon store options

---
+++ src/components/ui/Icon.vue
@@ -0,0 +1,30 @@
+
+<template lang="pug">
+  svg(:class="iconClass")
+    use(xmlns:xlink="http://www.w3.org/1999/xlink", :xlink:href="name")
+</template>
+
+<script>
+  export default {
+    name: 'MyComponent',
+    props: {
+      name: String
+    },
+    computed: {
+      iconClass () {
+        let iconClass = { 'icon': true };
+
+        iconClass[`icon--${this.name}`] = true;
+
+        return iconClass
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .icon {
+    fill: #000;
+  }
+</style>
---
+++ src/components/ui/IconStore.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="icon-store">
+    <img svg-inline svg-sprite src="@/icons/google.svg" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IconStore'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>

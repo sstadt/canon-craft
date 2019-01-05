9a14d4bc234e66b71b62f63d1e78a9a13776d4f5
Scott Stadt <scott.stadt@gmail.com>
2019-01-05 15:18:25 -0500

    progress commit; added wysiwyg icons, started styling controls

--- src/components/ui/IconStore.vue
+++ src/components/ui/IconStore.vue
@@ -6,7 +6,15 @@
       <g id="icon-compass-rose"><img svg-inline src="@/assets/icons/compass-rose.svg" /></g>
       <g id="icon-quill"><img svg-inline src="@/assets/icons/quill.svg" /></g>
       <g id="icon-cog"><img svg-inline src="@/assets/icons/cog.svg" /></g>
-      <g id="icon-quest"><img svg-inline src="@/assets/icons/quest.svg" /></g>
+      <g id="icon-bold"><img svg-inline src="@/assets/icons/bold.svg" /></g>
+      <g id="icon-italic"><img svg-inline src="@/assets/icons/italic.svg" /></g>
+      <g id="icon-underline"><img svg-inline src="@/assets/icons/underline.svg" /></g>
+      <g id="icon-strikethrough"><img svg-inline src="@/assets/icons/strikethrough.svg" /></g>
+      <g id="icon-h2"><img svg-inline src="@/assets/icons/h2.svg" /></g>
+      <g id="icon-h3"><img svg-inline src="@/assets/icons/h3.svg" /></g>
+      <g id="icon-ordered-list"><img svg-inline src="@/assets/icons/ordered-list.svg" /></g>
+      <g id="icon-unordered-list"><img svg-inline src="@/assets/icons/unordered-list.svg" /></g>
+      <g id="icon-link"><img svg-inline src="@/assets/icons/link.svg" /></g>
     </defs>
   </svg>
 </template>
--- src/components/ui/Wysiwyg.vue
+++ src/components/ui/Wysiwyg.vue
@@ -2,12 +2,25 @@
 <template lang="pug">
   .wysiwyg
     editor-menu-bar(:editor="editor")
-      div(slot-scope="{ commands, isActive }")
-        button.button.button--wysiwyg(:class="{ 'is-active': isActive.bold() }", @click="commands.bold") Bold
-        button.button.button--wysiwyg(:class="{ 'is-active': isActive.italic() }", @click="commands.italic") Italic
-        button.button.button--wysiwyg(:class="{ 'is-active': isActive.heading({ level: 2 }) }", @click="commands.heading({ level: 2 })") H2
-        button.button.button--wysiwyg(:class="{ 'is-active': isActive.heading({ level: 3 }) }", @click="commands.heading({ level: 3 })") H3
-        button.button.button--wysiwyg(:class="{ 'is-active': isActive.link() }", @click="commands.link") Link
+      .wysiwyg__controls(slot-scope="{ commands, isActive }")
+        .wysiwyg__control-group
+          button.button.button--wysiwyg(:class="{ 'is-active': isActive.bold() }", @click="commands.bold")
+            span.u-hidden Bold
+            icon(name="bold", :size="iconSize")
+          button.button.button--wysiwyg(:class="{ 'is-active': isActive.italic() }", @click="commands.italic")
+            span.u-hidden Italic
+            icon(name="italic", :size="iconSize")
+        .wysiwyg__control-group
+          button.button.button--wysiwyg(:class="{ 'is-active': isActive.heading({ level: 2 }) }", @click="commands.heading({ level: 2 })")
+            span.u-hidden H2
+            icon(name="h2", :size="iconSize")
+          button.button.button--wysiwyg(:class="{ 'is-active': isActive.heading({ level: 3 }) }", @click="commands.heading({ level: 3 })")
+            span.u-hidden H3
+            icon(name="h3", :size="iconSize")
+        .wysiwyg__control-group
+          button.button.button--wysiwyg(:class="{ 'is-active': isActive.link() }", @click="commands.link")
+            span.u-hidden Link
+            icon(name="link", :size="iconSize")
     editor-content(:editor="editor")
 </template>
 
@@ -25,7 +38,8 @@
     components: { EditorContent, EditorMenuBar, Icon },
     data () {
       return {
-        editor: null
+        editor: null,
+        iconSize: '16px'
       }
     },
     mounted () {
@@ -36,7 +50,7 @@
           new Heading({ levels: [2, 3] }),
           new Link()
         ],
-        content: '',
+        content: 'The local magistrate has been having trouble with kobolds nearby. He\'s offered a reward in gold for anyoone who can clear out their nest.',
         onUpdate: this.update
       })
     },
@@ -55,4 +69,17 @@
 </script>
 
 <style scoped lang="scss">
+  .wysiwyg {
+    &__controls {
+      display: flex;
+    }
+
+    &__control-group {
+      display: flex;
+
+      &:not(:last-child) {
+        margin-right: 10px;
+      }
+    }
+  }
 </style>
